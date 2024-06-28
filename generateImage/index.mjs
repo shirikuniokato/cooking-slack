df61f3c04a8be2abb3dd3047db6ff89477b2665b
shirikuniokato <rikuto.511@docomo.ne.jp>
2024-06-28 17:51:24 +0900

    gazou

--- generateImage/index.mjs
+++ generateImage/index.mjs
@@ -4,7 +4,9 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 
 export const handler = async (event) => {
-  const { prompt, size, channelId, user } = JSON.parse(event.Records[0].body);
+  const { prompt, size, style, channelId, user } = JSON.parse(
+    event.Records[0].body
+  );
 
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_TOKEN,
@@ -18,6 +20,7 @@ export const handler = async (event) => {
       prompt,
       n: 1,
       size,
+      style,
     });
     const imageUrl = response.data[0].url;
 
@@ -33,13 +36,13 @@ export const handler = async (event) => {
     const buffer = Buffer.from(base64Image, "base64");
     const fileName = `generated_image_${uuidv4()}.png`;
 
-    const msg = `<@${user}> さん
-画像の生成が完了したわぁ☆
+    const msg = `画像の生成が完了したわぁ☆
 どうかしら？私の改竄力であなたの希望通りのものにしてあげたわぁ♪
     
 # 画像詳細
 ${prompt}
-サイズ：${size}`;
+サイズ：${size}
+スタイル：${style}`;
 
     // Slackに画像をアップロード
     await client.files.uploadV2({
@@ -53,7 +56,9 @@ ${prompt}
     console.error(error);
     await client.chat.postMessage({
       channel: channelId,
-      text: `<@${user}> さん、画像の生成に失敗したようだわぁ☆`,
+      text: `画像の生成に失敗したようだわぁ☆
+# error
+${JSON.stringify(error)}`,
     });
   }
 };
